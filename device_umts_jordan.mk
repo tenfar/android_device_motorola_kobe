2469d7f4770b4f0c0f10beb92bb85fc04de4e759
bandroidx <bandroidx@gmail.com>
2011-02-19 00:51:55 -0800

    just changed names, but looks ok

--- device_umts_jordan.mk
+++ device_umts_jordan.mk
@@ -3,13 +3,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/motorola/umts_jordan/umts_jordan-vendor.mk)
+$(call inherit-product-if-exists, vendor/motorola/umts_kobe/umts_kobe-vendor.mk)
 
-DEVICE_PACKAGE_OVERLAYS += device/motorola/umts_jordan/overlay
+DEVICE_PACKAGE_OVERLAYS += device/motorola/umts_kobe/overlay
 
 
 ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/motorola/umts_jordan/kernel
+	LOCAL_KERNEL := device/motorola/umts_kobe/kernel
 else
 	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
 endif
