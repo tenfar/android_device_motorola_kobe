7d78eae2b227c6b9c6d956cee89f37caccdca54e
bandroidx <bandroidx@gmail.com>
2011-02-19 00:54:59 -0800

    Edited device_umts_jordan.mk via GitHub

--- device_umts_jordan.mk
+++ device_umts_jordan.mk
@@ -20,5 +20,5 @@ PRODUCT_COPY_FILES += \
 $(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
-PRODUCT_NAME := generic_umts_jordan
-PRODUCT_DEVICE := umts_jordan
+PRODUCT_NAME := generic_umts_kobe
+PRODUCT_DEVICE := umts_kobe
